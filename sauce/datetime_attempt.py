a2210553a95125a566da58b7682d9860c9e87527
Natalya Waters <natalyawaters2@gmail.com>
2019-11-21 11:14:16 +0000

    Date time attempt to loop csv files

---
+++ sauce/datetime_attempt.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from datetime import date, time, datetime
+
+
+
+    
+date = pd.date_range(start= '1/1/2013', end = '1/1/2014', freq= '0h2min', )
+print (date)
+
+DataFrame ={}
+for key in DataFrame: 
+    if date in DataFrame :
+        date+=1
+    else:
+        print(DataFrame)
+            
+
+
+
+#for data in csv:
+#    csv.count() 
+#    if csv.count()>10 :
+ #       print csv.count()
+  #  else:
+   #     skip 
+        
+
+
+
+
+
